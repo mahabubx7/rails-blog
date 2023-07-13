b4ee55136ccec1c7979b05ea0a45af1da05f5eaa
Mahabub Alam <mahabubx7@gmail.com>
2023-07-14 01:16:31 +0600

    validations(models): :robot: added-validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,8 +2,14 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :comments, foreign_key: :post_id
   has_many :likes, foreign_key: :post_id
-  before_save :update_post_counter
+  after_save :update_post_counter
   scope :get_5_comments, ->(post) { post.comments.order('created_at DESC').limit(5) }
+
+  validates :title, presence: true
+  validates :author, presence: true
+  validates :title, length: { in: 1..250 }
+  validates :likes_counter, numericality: { only_integer: true }, comparison: { greater_than_or_equal_to: 0 }
+  validates :comment_counter, numericality: { only_integer: true }, comparison: { greater_than_or_equal_to: 0 }
 
   def update_post_counter
     p = User.find_by_id(author.id)
