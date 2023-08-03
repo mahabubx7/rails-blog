81d2f39417fedcf4045d14472d2bfb2dce23656c
Mahabub Alam <mahabubx7@gmail.com>
2023-08-03 23:48:29 +0600

    cancancan (setup): :hammer: added-cancancan-in-project

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,6 +4,8 @@ class Comment < ApplicationRecord
 
   validates :text, presence: true
 
+  before_destroy -> { Post.find_by(id: post_id).decrement!(:comments_counter) }
+
   after_save do
     post = Post.find_by(id: post_id)
     post.increment!(:comments_counter)
--- app/models/post.rb
+++ app/models/post.rb
@@ -7,6 +7,7 @@ class Post < ApplicationRecord
   validates :comments_counter, comparison: { greater_than_or_equal_to: 0 }
   validates :likes_counter, comparison: { greater_than_or_equal_to: 0 }
 
+  before_destroy -> { User.find_by(id: author.id).decrement!(:posts_counter) }
   before_save -> { User.find_by(id: author.id).increment!(:posts_counter) }
 
   def five_most_recent_comments
