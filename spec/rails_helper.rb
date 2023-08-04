37487b9c755b4d215d3de915b7f49613f97f7538
Mahabub Alam <mahabubx7@gmail.com>
2023-08-04 19:01:48 +0600

    docs(setup): installed-rswag-and-setup

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,23 +3,9 @@ require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 # Prevent database truncation if the environment is production
-abort('The Rails environment is running in production mode!') if Rails.env.production?
+abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
-require 'webdrivers'
-Webdrivers::Chromedriver.required_version = '114.0.5735.90'
-# require 'capybara/rails'
-# require 'capybara/rspec'
-
-# Capybara.register_driver :selenium_chrome do |app|
-#   capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
-#     chromeOptions: { args: %w[headless disable-gpu no-sandbox] }
-#   )
-#   Capybara::Selenium::Driver.new(app, browser: :chrome, desired_capabilities: capabilities)
-# end
-
-# Capybara.javascript_driver = :selenium_chrome
-# Capybara.server = :puma
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
@@ -45,7 +31,7 @@ rescue ActiveRecord::PendingMigrationError => e
 end
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{Rails.root}/spec/fixtures"
+  config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
